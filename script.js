f77bd10e9f0aee0ff35f0a6d6a3ad39d1a735b60
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2021-07-22 13:28:18 +0100

    new styles applied

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
- let formctrl = document.querySelector('#Data');
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let formctrl = document.querySelector('#Data');
  let fullName = document.getElementById('fullName');
  let subject = document.getElementById('subject');
  let subjectBody = document.getElementById('sub-body');
  let commentList = document.querySelector('.output-container')
+const landingpage = document.querySelector('#thanks-remark');
+    
 
+function template(data){
+    
 
- function template(data){
+    let containerDiv = document.createElement('div');
+    containerDiv.innerHTML=`  <div>
+            
+            <h3>Author's Name: <small>${data.fullName}</small></h3>
+             </div>
+             <div>
 
-commentList.insertAdjacentHTML('beforeend',`
-             <fieldset>
-             <legend>Name</legend>
-            <h3> ${data.fullName}</h3>
-             </fieldset>
-             <fieldset>
-             <legend>subject/topic</legend>
-         <h3> ${data.subject}</h3>
-             </fieldset>
+         <h3>Title: <small>${data.subject}</small></h3>
+             </div>
 
-             <fieldset>
-           <legend>The push</legend>
+             <div>
+             <h4>ARTICLE</h4>
              <p>${data.subjectBody}</p>
-            </fieldset>
+            </div>
+            <hr>`;
+
+
+
+
+commentList.appendChild(containerDiv);
+
+// commentList.insertAdjacentHTML('beforeend',`
+//              <fieldset>
+//              <legend>Name</legend>
+//             <h3> ${data.fullName}</h3>
+//              </fieldset>
+//              <fieldset>
+//              <legend>subject/topic</legend>
+//          <h3> ${data.subject}</h3>
+//              </fieldset>
+
+//              <fieldset>
+//            <legend>The push</legend>
+//              <p>${data.subjectBody}</p>
+//             </fieldset>
             
 
-            `)
+//             `)
  }
 
  function appendComment(e){
@@ -48,6 +78,9 @@ commentList.insertAdjacentHTML('beforeend',`
       subject.value ="";
       subjectBody.value="";
 
+      
+      commentList.style.width='100%';
+
     //   save the advice to localstorage ( textContent make it display text not html tags when yuo use innerHTML in LocalStorage)
     localStorage.setItem('comments', commentList.innerHTML);
  }
@@ -60,78 +93,23 @@ const saved = localStorage.getItem('comments');
 if(saved){
     commentList.innerHTML=saved;
 }
-// this will clear the localstorage right from the browser
+
+
+
+    document.querySelector('.advicebtn').addEventListener('click', (e)=>{
+        e.preventDefault();
+landingpage.classList.add('form-hiden');
+formctrl.classList.remove('form-hiden');
+    })
+
+        document.querySelector('.backbtn').addEventListener('click', (e)=>{
+            e.preventDefault();
+landingpage.classList.remove('form-hiden');
+formctrl.classList.add('form-hiden');
+    })
+})
+ // this will clear the localstorage right from the browser
 function clear(){
    localStorage.clear();
     location.reload();
 }
-
-
-// I LEARNED WHY I SHOULD NOT DO IT THIS WAY
-// function doFirst(){
-//     let formctrl = document.querySelector('#Data');
-//             // let fullName = document.getElementById('fullName');
-//             // let subject = document.getElementById('subject');
-//             // let subjectBody = document.getElementById('sub-body');
-//         formctrl.addEventListener('submit', addForm,false);
-//         displayData()
-//         }
-        
-//         //  console.log(localStorage.getItem('formData'));
-//         function addForm(e){
-//             e.preventDefault();
-            
-//             let formctrl = document.querySelector('#Data');
-//             let fullName = document.getElementById('fullName');
-//             let subject = document.getElementById('subject');
-//             let subjectBody = document.getElementById('sub-body');
-
-//             let formData = {
-//                 fullName: fullName.value,
-//                 subject:subject.value,
-//                 subjectBody:subjectBody.value
-//               }
-//             localStorage.setItem('formData',JSON.stringify(formData));
-//             displayData();
-//             formctrl="";
-//             fullName="";
-//             subject="";
-
-//         }
-
-//         function displayData(){
-//             if(localStorage.getItem('formData')){
-           
-//             // console.log(JSON.parse(localStorage.getItem('formData')));
-//             // destructure of the object
-//             let {fullName,subject,subjectBody}=JSON.parse(localStorage.getItem('formData'));
-//             let output = document.querySelector('.output-container');
-//             output.innerHTML=`
-//             <fieldset>
-//             <legend>Name</legend>
-//            <h3> ${fullName}</h3>
-//             </fieldset>
-//             <fieldset>
-//             <legend>subject/topic</legend>
-//            <h3> ${subject}</h3>
-//             </fieldset>
-
-//             <fieldset>
-//             <legend>The push</legend>
-//             <p>${subjectBody}</p>
-//             </fieldset>
-            
-
-//             `;
-        
-            
-           
-// }
-//         }
-    
-
-// function clear(){
-//     localStorage.clear();
-//     location.reload();
-// }
-//         window.addEventListener('load',doFirst,false);
